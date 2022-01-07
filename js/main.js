8655aac57d61181dde450af12ba5813e69232fa5
tjsrbKR <kkil5205@gmail.com>
2022-01-07 22:45:04 +0900

    프로젝트 시작

---
+++ js/main.js
@@ -0,0 +1,7 @@
+const $categoryBtn = document.getElementsByClassName("categoryBtn");
+for(let i =0; i<categoryBtn.length;i++){
+    categoryBtn[i].addEventListener('click',asd);
+}
+function asd(){
+    alert('asd');
+}
