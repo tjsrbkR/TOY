f1e22d4c8c78a1799e5a2ed045fff44fc2308e63
tjsrbKR <kkil5205@gmail.com>
2022-01-11 03:29:25 +0900

    이미지 선택오류

---
+++ data/data.js
@@ -0,0 +1,90 @@
+    const qnstlr = [
+        {
+            'id' : "1",
+            'name' : "떡볶이",
+            'value':"분식",
+            'img':"https://recipe1.ezmember.co.kr/cache/recipe/2019/01/17/6fb9bfa566c0c3fec18080b5e927c2661.jpg"
+            
+        },
+        {
+            'id' : "2",
+            'name' : "핫도그",
+            'value':"분식",
+            'img':"https://i.ytimg.com/vi/xMTb669y2Ko/maxresdefault.jpg"
+        }
+]
+const gkstlr = [ 
+    {
+        'id':"1",
+        'name':"김밥",
+        'value':"한식",
+        'img':"https://recipe1.ezmember.co.kr/cache/recipe/2016/02/21/f34c2f0fcd67513941d683d90050f3c01.jpg"
+
+    },{
+        'id':"2",
+        'name': "비빔밥",
+        'value':"한식",
+        'img':"https://health.chosun.com/site/data/img_dir/2021/01/27/2021012702508_0.jpg"
+    }
+]
+const wndtlr = [
+    {
+        'id':"1",
+        'name':"짬뽕",
+        'value':"중식",
+        'img':"https://img-cf.kurly.com/shop/data/goodsview/20210311/gv30000164603_1.jpg"
+
+    },{
+        'id':"2",
+        'name': "짜장면",
+        'value':"중식",
+        'img':"https://i.ytimg.com/vi/Yn8ZFTBCJJQ/maxresdefault.jpg"
+
+    }
+]
+
+const dlftlr = [
+    {
+        'id':"1",
+        'name':"초밥",
+        'value':"일식",
+        'img':"http://th1.tmon.kr/thumbs/image/a3c/e19/bc5/a48971364_700x700_95_FIT.jpg"
+    },{
+        'id':"2",
+        'name': "라멘",
+        'value':"일식",
+        'img':"https://imgcp.aacdn.jp/img-a/1720/auto/global-aaj-front/article/2018/08/5b7e023328857_5b7e02117ec3f_1869520439.jpg"
+
+    }
+]
+
+const ditlr = [
+    {
+        'id':"1",
+        'name':"치킨",
+        'value':"야식",
+        'img':"https://img.khan.co.kr/news/2021/05/09/l_2021051001000864600073041.jpg"
+    },
+    {
+        'id':"2",
+        'name': "족발",
+        'value':"야식",
+        'img':"https://blog.kakaocdn.net/dn/WbMEr/btq329TgSg3/VXkAaCxbs1XbkHjDPQAWv1/img.png"
+    }
+]
+
+const didtlr =[
+    {
+        'id':"1",
+        'name':"파스타",
+        'value':"양식",
+        'img':"https://sjnfzdfjrjgl1655541.cdn.ntruss.com/goods/3/2019/07/158_tmp_d0edbc1371ecb3575266e8215e5e16961570view.jpg"
+    },
+    {
+        'id':"2",
+        'name': "스테이크",
+        'value':"양식",
+        'img':"https://recipe1.ezmember.co.kr/cache/recipe/2017/01/06/43c256e93d9c18240a3c5ed7ee3f55051.jpg"
+        
+    }
+]
