5e0d3fca264035ff22978a727171ec8c2dc4c039
tjsrbKR <kkil5205@gmail.com>
2022-01-17 22:38:50 +0900

    데이터 추가

--- data/data.js
+++ data/data.js
@@ -11,6 +11,14 @@
             'name' : "핫도그",
             'value':"분식",
             'img':"https://i.ytimg.com/vi/xMTb669y2Ko/maxresdefault.jpg"
+            
+        },
+        {
+            'id' : "3",
+            'name' : "순대",
+            'value':"분식",
+            'img':"https://images.chosun.com/resizer/8FYKlbcqgUK9QeRIi3rLkndbShk=/616x0/smart/cloudfront-ap-northeast-1.images.arcpublishing.com/chosun/5WUPVB2RORDZJMWE4BE7HF4H2I.jpg"
+            
         }
 ]
 const gkstlr = [ 
@@ -25,7 +33,15 @@ const gkstlr = [
         'name': "비빔밥",
         'value':"한식",
         'img':"https://health.chosun.com/site/data/img_dir/2021/01/27/2021012702508_0.jpg"
+    },
+    {
+        'id':"3",
+        'name': "오므라이스",
+        'value':"한식",
+        'img':"https://recipe1.ezmember.co.kr/cache/recipe/2016/06/05/1ba7e34cf0daf694f09a3a9539ebdb161.jpg"
     }
+
+    
 ]
 const wndtlr = [
     {
@@ -41,6 +57,13 @@ const wndtlr = [
         'img':"https://i.ytimg.com/vi/Yn8ZFTBCJJQ/maxresdefault.jpg"
 
     }
+    ,{
+        'id':"3",
+        'name': "탕수육",
+        'value':"중식",
+        'img':"https://recipe1.ezmember.co.kr/cache/recipe/2020/07/05/2e0e7c019f283bcc36d34cdee876d15b1.jpg"
+
+    }
 ]
 
 const dlftlr = [
@@ -54,6 +77,12 @@ const dlftlr = [
         'name': "라멘",
         'value':"일식",
         'img':"https://imgcp.aacdn.jp/img-a/1720/auto/global-aaj-front/article/2018/08/5b7e023328857_5b7e02117ec3f_1869520439.jpg"
+    }
+    ,{
+        'id':"3",
+        'name': "규동",
+        'value':"일식",
+        'img':"https://recipe1.ezmember.co.kr/cache/recipe/2015/11/27/39e04864a912c74502934fda45789e471.jpg"
 
     }
 ]
@@ -71,6 +100,13 @@ const ditlr = [
         'value':"야식",
         'img':"https://blog.kakaocdn.net/dn/WbMEr/btq329TgSg3/VXkAaCxbs1XbkHjDPQAWv1/img.png"
     }
+    ,
+    {
+        'id':"3",
+        'name': "닭발",
+        'value':"야식",
+        'img':"http://th3.tmon.kr/thumbs/image/053/cc5/40a/8d1dbd2d4_700x700_95_FIT.jpg"
+    }
 ]
 
 const didtlr =[
@@ -85,6 +121,14 @@ const didtlr =[
         'name': "스테이크",
         'value':"양식",
         'img':"https://recipe1.ezmember.co.kr/cache/recipe/2017/01/06/43c256e93d9c18240a3c5ed7ee3f55051.jpg"
+        
+    }
+    ,
+    {
+        'id':"3",
+        'name': "부리또",
+        'value':"양식",
+        'img':"https://i.ytimg.com/vi/kD7vxraoWrA/maxresdefault.jpg"
         
     }
 ]
