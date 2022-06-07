0816bdfc691599def93827121536806abf89cd24
tjsrbKR <kkil5205@gmail.com>
2022-06-07 17:10:19 +0900

    계산기

--- Calculator-toy/js/index.js
+++ Calculator-toy/js/index.js
@@ -1,132 +1,122 @@
-const Calculator = document.querySelector(".Calculator");
-let firstValue = [];
-let SecondValue = [];
-let flag;
-let coin =false;  
-let totalresult  =0; 
-    //eval 함수는 절대로 쓰지 말자
-    //eval 함수는 스크립트 실행문을 만들어서 실행시킨다. 보안에 매우 취약함
-const btn = document.getElementsByTagName("button");
-const nowInput = document.getElementById("nowInput");
-const result = document.querySelector("#result");
-    const Btndelete = document.getElementById("delete");
-    Btndelete.addEventListener("click", function () {
-        result.value = result.value.slice(0, -1);
-        nowInput.value = nowInput.value.slice(0, -1);
-    });
+const calcBody = document.querySelectorAll(".calc-body");
+const calcValue = document.querySelector(".calc-value");
 
-    const operatorClicked = (e) => {
-    firstValue.push(parseFloat(result.value));
-    coin = !coin; 
-    result.value = '';
-    const opButton = e.target; // 연산자 버튼임
-    nowInput.value += e.target.value;
-    if (flag) {
-        msg(); // 이미 true인데 또 연산자를 누름
-        return;
-    }
-    else{
-        flag = true; // "op" 값 바인딩
-    }
-
+const calcValueObj = {
+  currentValue: 0,
+  temp: 0,
+  operator: ""
 };
 
-
-const btnOperators = document.querySelectorAll(
-    `.Calculator [data-type="op"]`
-    );
-    btnOperators.forEach((btn) => {
-    // 모든 연산자 버튼에 클릭 콜백을 걸어줌
-    btn.addEventListener("click", operatorClicked, true);
-});
-
-
-
-const NumClicked = (e) =>{
-    const NumButton = e.target;
-    flag = false;
-    nowInput.value += e.target.value;
-    result.value += e.target.value;
-};
-const btnNumber = document.querySelectorAll(
-    `.Calculator [data-type="digit"]`
-    );
-    btnNumber.forEach((btnNum) => {
-    btnNum.addEventListener("click", NumClicked,false);
-
-});
-function msg() {
-    if (flag) {
-        nowInput.value = nowInput.value.slice(0, -1);
-        alert("연산자를 한번만 입력 하세요");
-    }
+function setNumber(value) {
+  if (calcValueObj.operator) {
+    // 계산할 기호가 있으면 저장
+    const result = String(calcValueObj.temp) + value;
+    console.log(result);
+    calcValueObj.temp = Number(result);
+    renderNumber(calcValueObj.temp);
+  } else {
+    // 계산 중이 아니면 현재 벨류
+    const result = String(calcValueObj.currentValue) + value;
+    calcValueObj.currentValue = Number(result);
+    console.log(calcValueObj.currentValue);
+    renderNumber(calcValueObj.currentValue);
+  }
 }
 
-const BtnTotal = document.getElementById("total").addEventListener("click",()=>{
-    SecondValue.push(parseFloat(result.value));
-    const btnNumber = document.querySelectorAll(
-        `.Calculator [data-type="op"]`
-        );
-        for(let i = 0; i < btnNumber.length; i++){
-        switch(btnNumber[i].value){
-            case "+":
-                clear();
-                totalresult = firstValue[0] + SecondValue[0];
-                firstValue.push(totalresult);
-                nowInput.value += totalresult;
-                result.value += totalresult;
-                break;
-            console.log(`This Item is : "${btnNumber[i].value}"`);
-            case "-": 
-                clear();
-                console.log(firstValue[0]);
-                console.log(SecondValue[0]);
-                firstValue.push(totalresult);
-                totalresult = firstValue[0] - SecondValue[0];
-                console.log(totalresult);
-                nowInput.value === totalresult;
-                result.value === totalresult;
-                break;
-            // case 'x':
-            //     totalresult = firstValue[0] * SecondValue[0];
-            //     clear();
-            //     firstValue.push(totalresult);
-            //     nowInput.value += totalresult;
-            //     result.value += totalresult;
-            //     break;
-            // case '÷':
-            //     totalresult = firstValue[0] / SecondValue[0];
-            //     clear();
-            //     firstValue.push(totalresult);
-            //     nowInput.value += totalresult;
-            //     result.value += totalresult;
-            //     break;
-        }
-        const arr = ["+", "-", "*", "/"];
+function setOperation(operator) {
+  if (calcValueObj.currentValue && calcValueObj.temp) {
+    // 연산자가 있으면 계산하고 다음 연산자를 집어넣어야 한다.
+    calculator();
+    renderNumber(calcValueObj.currentValue);
+  }
+  calcValueObj.operator = operator;
+}
 
-// for (let i = 0; i < arr.length; i++) {
-//     console.log(`This Item is : "${arr[i]}"`)
-//     switch (arr[i]) {
-//         case  '+' :
-//             console.log('Plus!!');
-//             break;
-//         case  '-' :
-//             console.log('Minus!!');
-//             break;
+function reset() {
+  calcValueObj.currentValue = 0;
+  calcValueObj.temp = 0;
+  calcValueObj.operator = "";
+  renderNumber(0);
+}
 
-//         case  '*' :
-//             console.log('곱하기!!');
-//             break;
-//         case  '/' :
-//             console.log('나누기!!');
-//             break;
-//     }
-// }
+function finish() {
+  calculator();
+  renderNumber(calcValueObj.currentValue);
+  calcValueObj.operator = "";
+}
+
+function renderNumber(number) {
+  if (calcValue) {
+    calcValue.textContent = number;
+  }
+}
+
+function handleClickBtn(e) {
+  const { value } = e.target;
+
+  // 숫자라면
+  if (/[0-9]/.test(value)) { // /[0-9]/ >> 숫자 정규식
+    return setNumber(value);
+  }
+
+  // 초기화라면
+  if (value === "AC") {
+    return reset();
+  }
+
+  // 계산 끝
+  if (value === "=") {
+    return finish();
+  }
+
+  // 계산하는 값들이라면
+  return setOperation(value);
+}
+
+function init() {
+  // calcBody 안에 있는 버튼에 클릭 이벤트 위임
+
+  calcBody.forEach((button) => {
+    button.addEventListener("click", handleClickBtn);
+  });
+
+  // 초기 계산기 출력 값 0
+  renderNumber(0);
+}
+
+// 초기 진입점
+init();
+
+function calculator() {
+  switch (calcValueObj.operator) {
+    case "+": {
+      calcValueObj.currentValue += calcValueObj.temp;
+      break;
     }
-
-});
-    function clear(){
-        result.value = '';
-        nowInput.value ='';
-        totalresult = 0;
+    case "*": {
+      calcValueObj.currentValue *= calcValueObj.temp;
+      break;
     }
+    case "-": {
+      calcValueObj.currentValue -= calcValueObj.temp;
+      break;
+    }
+    case "/": {
+      calcValueObj.currentValue /= calcValueObj.temp;
+      break;
+    }
+    case "%": {
+      calcValueObj.currentValue %= calcValueObj.temp;
+      break;
+    }
+    case "^": {
+      calcValueObj.currentValue **= calcValueObj.temp;
+      break;
+    }
+    default: {
+      break;
+    }
+  }
+  // 계산 후 초기화
+  calcValueObj.temp = 0;
+}
